2e4a46a6c47201ec0b986a4c57ff6641efea3874
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2021-09-09 09:59:54 -0500

    simplified sample_organizer script

--- sample_organizer.py
+++ sample_organizer.py
@@ -1,8 +1,7 @@
 
 
 from pathlib import Path
-from pprint import pprint 
-# import shutil
+import shutil
 
 path_complete_dataset = Path(r"Z:\Kayvan\Human Data".replace("\\",'/'))
 list_all_files = list(path_complete_dataset.glob("*"))
@@ -14,65 +13,47 @@ path_output = Path(r"Z:\0-Projects and Experiments\KS - OCT membranes\human_data
 list_no_samples_found = []
 list_no_oct_subsamples = []
 
-for num_sample, sample in enumerate(list_samples[0:1], start=1) :
+for num_sample, sample in enumerate(list_samples, start=1) :
+    pass
     print(f"{num_sample}/{len(list_samples)}  |  {sample.name}")
     
     path_inflation = sample / "Inflation"
-    
-    path_output_inflation = path_output / "Inflation"
-    
-    ## Make dir
-    # path_output_inflation.mkdir(parents=True)
-    
+       
     list_subsamples = list(path_inflation.glob("*"))
     
     if len(list_subsamples) == 0:
         print("no samples found")
         list_no_samples_found.append(sample)
-        
     
-    for subsample in list_subsamples:
+    for num_subsample, subsample in enumerate(list_subsamples, start=1):
         pass
+        print(f"{num_subsample}/{len(list_subsamples)}  |  {subsample.name}")
         
-        # mkdir subsample
-        path_output_subsample = path_output_inflation / subsample.name
-        # path_output_subsample.mkdir()
+        # mkdir subsample dir and parents
+        path_output_subsample = path_output / sample.name /"Inflation" / subsample.name
+        path_output_subsample.mkdir(parents=True, exist_ok=True)
     
         ## copy oct files 
-        list_path_subsample_oct_tiffs = list(subsample.glob("*Mode2D.tiff"))
-        
-         
+        list_path_subsample_oct_tiffs = list(subsample.glob("*Mode2D.tiff"))  
         if len(list_path_subsample_oct_tiffs) == 0:
             print("no oct samples found")
             list_no_oct_subsamples.append(subsample)
     
-        for file in list_path_subsample_oct_tiffs:
+        for path_file in list_path_subsample_oct_tiffs:
             pass
-            src = file
-            dst = path_output_subsample / file.name
-            #shutil.copy2()
+            if not (path_output_subsample /  path_file.name).exists():
+                    print(f"copying: {path_file}")
+                    # shutil.copy2(path_file, path_output_subsample)
         
         # copy txt files 
         list_path_subsample_pressure_files = list(subsample.glob("*.txt"))
-        for file in list_path_subsample_pressure_files:
+        for path_file in list_path_subsample_pressure_files:
             pass
-            src = file
-            dst = path_output_subsample / file.name
-            # shutil.copy2()
+            if not (path_output_subsample /  path_file.name).exists():
+                print(f"copying: {path_file}")
+                # shutil.copy2(path_file, path_output_subsample)
 
-        ####
-        
-        print(f"{'-'*10} {sample.name}" )
-        print("oct files:")
-        pprint(list_path_subsample_oct_tiffs)
-        print("pressure files:")
-        pprint(list_path_subsample_pressure_files)
-        
-        #copy files --> check if they are already in the folder
-        #  make a dir for each sample
-        # from each dir, copy 
     
     
-    
-    
+    #%%
     
