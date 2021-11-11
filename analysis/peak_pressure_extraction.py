475f154b5c0f36ada1251c80c958e7000e0e69e7
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2021-11-10 18:57:57 -0600

    finished script for peak pressure extraction

---
+++ analysis/peak_pressure_extraction.py
@@ -0,0 +1,86 @@
+
+
+from pathlib import Path
+import re
+import pandas as pd
+from tqdm import tqdm
+
+path_dataset = Path(r"Z:\0-Projects and Experiments\KS - OCT membranes\human_dataset_copy_no_oct_files")
+
+list_path_files = list(path_dataset.glob("*"))
+list_path_sample_dirs = [p for p in list_path_files if p.is_dir()]
+
+list_str_path_sample_dirs = [str(p) for p in list_path_sample_dirs]
+
+# #split into labored/unlabored(c section)
+# list_labored = []
+# list_unlabored = []
+
+# for path_str_subsample in list_str_path_sample_dirs:
+#     pass
+#     if "_C_section" in path_str_subsample:
+#         list_unlabored.append(path_str_subsample)
+#     else:
+#         list_labored.append(path_str_subsample)
+
+#%%
+
+dict_peak_pressures = {}
+
+### keys
+
+# (pericervical,periplacental)
+df = pd.DataFrame(columns=["sample", "subsample", "layers", "location", "pregnancy","max_pressure"])
+
+for path_sample in tqdm(list_path_sample_dirs):
+    pass
+    
+    sample = path_sample.stem
+    
+    path_subsample = path_sample / "Inflation"
+    path_all_files_subsample = list(path_subsample.glob("*"))
+    
+    list_str_all_files_subsample = [str(p) for p in path_all_files_subsample if p.is_dir()]
+    
+    for subsample_dir in list_str_all_files_subsample:
+        pass
+   
+        #get all files 
+        list_pressure_files = list(Path(subsample_dir).glob("*ressure*txt"))
+        list_early_2020_pressure_files = list(Path(subsample_dir).glob("*Mode2D.txt"))
+        
+        list_all_pressure_files = list_pressure_files + list_early_2020_pressure_files
+        
+        df_subsample_pressures = pd.DataFrame(columns=["date", "pressure"])
+        for file_pressure in list_all_pressure_files:
+            pass
+            df_temp = pd.read_csv(file_pressure,delimiter=";", names=["date", "pressure"])
+            df_subsample_pressures = df_subsample_pressures.append(df_temp)
+        
+        # variables 
+        max_pressure = df_subsample_pressures["pressure"].max()
+        # print(max_pressure)
+        
+        subsample_name = Path(subsample_dir).stem
+        
+        # location
+        if "pericervical" in subsample_name: location = "pericervical"
+        elif "periplacental" in subsample_name: location = "periplacental"
+        
+        # layers
+        if "amniochorion" in subsample_name: layers = "amniochorion"
+        elif "amnion" in subsample_name: layers = "amnion"
+        elif "chorion" in subsample_name: layers = "chorion"
+        
+        # labored/unlabored
+        pregnancy = "C_section" if "C_section" in subsample_name else "labored"
+        
+        data = {"sample": [sample], "subsample": [subsample_name], "location" : [location] , "layers": [layers], "pregnancy":pregnancy, "max_pressure": [max_pressure]}
+        df = df.append(pd.DataFrame(data=data))
+    
+    # amniochorion and pericervical
+    # list_path_amniochorion_pericervical = list(filter(re.compile("pericervical").search, list_path_amniochorion))[0]
+
+    
+    
+    
